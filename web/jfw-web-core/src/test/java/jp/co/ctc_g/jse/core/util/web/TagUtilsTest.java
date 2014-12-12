6bd8528ab45a5b4a72deaa6337b8feded10943a5
Tetsuo NAKAMURA <tetsuo.nakamura@ctc-g.co.jp>
2014-12-12 10:33:47 +0900

    Added support for test on Java 8.

--- web/jfw-web-core/src/test/java/jp/co/ctc_g/jse/core/util/web/TagUtilsTest.java
+++ web/jfw-web-core/src/test/java/jp/co/ctc_g/jse/core/util/web/TagUtilsTest.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,9 +17,16 @@
 package jp.co.ctc_g.jse.core.util.web;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertThat;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import jp.co.ctc_g.jfw.core.util.Arrays;
+import jp.co.ctc_g.jfw.core.util.Maps;
+import jp.co.ctc_g.jse.core.util.web.TagUtils.UrlType;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -27,17 +34,14 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockPageContext;
 import org.springframework.mock.web.MockServletContext;
-
-import jp.co.ctc_g.jfw.core.util.Arrays;
-import jp.co.ctc_g.jfw.core.util.Maps;
-import jp.co.ctc_g.jse.core.util.web.TagUtils.UrlType;
+import org.springframework.util.StringUtils;
 
 public class TagUtilsTest {
 
     private MockPageContext page;
     private MockHttpServletResponse response;
     private MockHttpServletRequest request;
-    
+
     @Before
     public void setUp() throws Exception {
         MockServletContext sc = new MockServletContext();
@@ -45,21 +49,21 @@ public class TagUtilsTest {
         response = new MockHttpServletResponse();
         page = new MockPageContext(sc, request, response);
     }
-    
+
     @Test
     public void 絶対パスとして判定される() {
         String path = "http://www.ctc-g.co.jp";
         UrlType type = TagUtils.getUrlType(path);
         assertThat(type, is(UrlType.ABSOLUTE));
     }
-    
+
     @Test
     public void コンテキストパスからの相対パスとして判定される() {
         String path = "/app/test";
         UrlType type = TagUtils.getUrlType(path);
         assertThat(type, is(UrlType.CONTEXT_RELATIVE));
     }
-    
+
     @Test
     public void の相対パスとして判定される() {
         String path = "test";
@@ -70,25 +74,54 @@ public class TagUtilsTest {
     @Test
     public void パスパラメータが置換される() throws Exception {
         String path = "app/{groupId}/{userId}";
-        Map<String, String[]> params = Maps.hash("groupId", Arrays.gen("test")).map("userId", Arrays.gen("20120001"));
-        String replacedPath = TagUtils.replaceUriTemplateParams(path, params, page);
+        Map<String, String[]> params = Maps.hash("groupId", Arrays.gen("test"))
+                .map("userId", Arrays.gen("20120001"));
+        String replacedPath = TagUtils.replaceUriTemplateParams(path, params,
+                page);
         assertThat(replacedPath, is("app/test/20120001"));
     }
 
     @Test
     public void クエリ文字列が生成される() throws Exception {
-        Map<String, String[]> params = Maps.hash("groupId", Arrays.gen("test")).map("userId", Arrays.gen("20120001"));
+        Map<String, String[]> params = Maps.hash("groupId", Arrays.gen("test"))
+                .map("userId", Arrays.gen("20120001"));
         String queryString = TagUtils.createQueryString(params, true, page);
         assertThat(queryString, is("?groupId=test&userId=20120001"));
     }
-    
+
     @Test
     public void URLが生成される() throws Exception {
         String path = "app/{groupId}/{userId}";
-        Map<String, String[]> params = Maps.hash("groupId", Arrays.gen("test")).map("userId", Arrays.gen("20120001"));
+        Map<String, String[]> params = Maps.hash("groupId", Arrays.gen("test"))
+                .map("userId", Arrays.gen("20120001"));
         params.put("q1", Arrays.gen("v1"));
         params.put("q2", Arrays.gen("v20", "v21", "v22"));
         String url = TagUtils.createUrl(path, params, page, true, true);
-        assertThat(url, is("app\\/test\\/20120001?groupId=test&amp;q2=v20q2=v21q2=v22&amp;q1=v1&amp;userId=20120001"));
+        assertThat(url, is(startsWith("app\\/test\\/20120001?")));
+        String sortedUrl = sortQueryString(url);
+        assertThat(
+                sortedUrl,
+                is("app\\/test\\/20120001?groupId=test&amp;q1=v1&amp;q2=v20q2=v21q2=v22&amp;userId=20120001"));
     }
+
+    private String sortQueryString(String uri) {
+        Pattern pattern = Pattern.compile("^([^\\?]*)(?:(\\?)(.*)?)$");
+        Matcher matcher = pattern.matcher(uri);
+        if (!matcher.matches()) {
+            return uri;
+        }
+
+        StringBuilder newQueryString = new StringBuilder(matcher.group(1));
+        if (matcher.groupCount() >= 2 && !StringUtils.isEmpty(matcher.group(2))) {
+            newQueryString.append(matcher.group(2));
+        }
+        if (matcher.groupCount() >= 3) {
+            String[] sortedPair = StringUtils
+                    .sortStringArray(matcher.group(3).split("&amp;"));
+            newQueryString.append(StringUtils
+                    .arrayToDelimitedString(sortedPair, "&amp;"));
+        }
+        return newQueryString.toString();
+    }
+
 }
